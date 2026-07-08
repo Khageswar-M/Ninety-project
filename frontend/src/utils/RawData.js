c2c1ed562991cd17f71d3a482151f8e109d1f2dd
Khageswar-M <ok3programmer@gmail.com>
2026-07-08 20:14:22 +0530

    Add RawData.js from feature/actions

---
+++ frontend/src/utils/RawData.js
@@ -0,0 +1,22 @@
+export const board = [
+        //00    01     02     03     04     05     06     07     08   
+        [true, true, true, true, false, false, false, false, false],
+        //10    11     12     13     14     15     16     17     18   
+        [false, false, false, false, false, false, false, false, false],
+        //20    21     22     23     24     25     26     27     28   
+        [false, false, false, false, false, false, false, false, false],
+        //30    31     32     33     34     35     36     37     38   
+        [false, false, false, false, false, false, false, false, false],
+        //40    41     42     43     44     45     46     47     48   
+        [false, false, false, false, false, false, false, false, false],
+        //50    51     52     53     54     55     56     57     58   
+        [false, false, false, false, false, false, false, false, false],
+        //60    61     62     63     64     65     66     67     68   
+        [false, false, false, false, false, false, false, false, false],
+        //70    71     72     73     74     75     76     77     78   
+        [false, false, false, false, false, false, false, false, false],
+        //80    81     82     83     84     85     86     87     88   
+        [false, false, false, false, false, false, false, false, false],
+        //90    91     92     93     94     95     96     97     98   
+        [false, false, false, false, false, false, false, false, false],
+    ];
