ffffe9e69039cbb6c2648f5a256268aa7f76ec11
Khageswar-M <ok3programmer@gmail.com>
2026-08-06 06:12:29 +0000

    Eamil service with service implementation added

---
+++ backend/src/main/java/Ninety/com/backend/service/EmailService.java
@@ -0,0 +1,9 @@
+package Ninety.com.backend.service;
+
+import java.util.concurrent.CompletableFuture;
+
+public interface EmailService{
+    CompletableFuture<Void> sendOtpEmail(String toEmail, String fullName, String otp);
+
+    CompletableFuture<Void> sendWelcomeEmail(String toEmail, String fullName);
+}
